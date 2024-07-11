f24f5417589adeb09928941a2a860782d055ea05
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-07-11 15:55:21 -0400

    added user input handling

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,8 +1,11 @@
 use chip8_core::*;
-use std::env;
-use sdl2::event::Event;
+
 use std::fs::File;
 use std::io::Read;
+use std::env;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
@@ -11,6 +14,7 @@ use sdl2::video::Window;
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+const TICKS_PER_FRAME: usize = 10;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -19,6 +23,7 @@ fn main() {
         return;
     }
 
+    // Setup SDL
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
@@ -45,36 +50,71 @@ fn main() {
     'gameloop: loop {
         for evt in event_pump.poll_iter() {
             match evt {
-                Event::Quit { .. } => {
+                Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..}=> {
                     break 'gameloop;
+                },
+                Event::KeyDown{keycode: Some(key), ..} => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, true);
+                    }
+                },
+                Event::KeyUp{keycode: Some(key), ..} => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, false);
+                    }
                 },
                 _ => ()
             }
-            chip8.tick();
-            draw_screan(&chip8, &mut canvas)
         }
+
+        for _ in 0..TICKS_PER_FRAME {
+            chip8.tick();
+        }
+        chip8.tick_timers();
+        draw_screen(&chip8, &mut canvas);
     }
 }
 
-fn draw_screan(emu: &Emu, canvas: &mut Canvas<Window>){
-
+fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>) {
     // Clear canvas screen
-    canvas.set_draw_color(Color::RGB(0,0,0));
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
 
     let screen_buf = emu.get_display();
     // Set draw color to white, iterate through each point and see if it should be drawn
-    canvas.set_draw_color(Color::RGB(255,255,255));
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
     for (i, pixel) in screen_buf.iter().enumerate() {
         if *pixel {
             // Convert 1D array's index into a 2D (x,y) position
             let x = (i % SCREEN_WIDTH) as u32;
-            let y = (i/SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
 
             // Draw rectangle at (x,y), scaled by SCALE value
-            let rect = Rect::new((x*SCALE) as i32, (y*SCALE) as i32, SCALE, SCALE);
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
             canvas.fill_rect(rect).unwrap();
         }
     }
     canvas.present();
+}
+
+fn key2btn(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 =>    Some(0x1),
+        Keycode::Num2 =>    Some(0x2),
+        Keycode::Num3 =>    Some(0x3),
+        Keycode::Num4 =>    Some(0xC),
+        Keycode::Q =>       Some(0x4),
+        Keycode::W =>       Some(0x5),
+        Keycode::E =>       Some(0x6),
+        Keycode::R =>       Some(0xD),
+        Keycode::A =>       Some(0x7),
+        Keycode::S =>       Some(0x8),
+        Keycode::D =>       Some(0x9),
+        Keycode::F =>       Some(0xE),
+        Keycode::Z =>       Some(0xA),
+        Keycode::X =>       Some(0x0),
+        Keycode::C =>       Some(0xB),
+        Keycode::V =>       Some(0xF),
+        _ =>                None,
+    }
 }
